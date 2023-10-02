1a454b1df2311774612c0a5b0850533480cbdc07
Friskky <brendanco7@hotmail.com>
2023-10-02 09:12:20 -0700

    version 1.0.0

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Friskky Developments'
+description 'Quick Humaine labs heist'
+version '1.0.0'
+
+client_scripts {
+	'client/*.lua'
+}
+server_script {
+    'server/*.lua'
+}
+shared_script {
+    'config.lua',
+}
+
+escrow_ignore {
+    "client/pdalerts.lua",
+    "config.lua"
+}
